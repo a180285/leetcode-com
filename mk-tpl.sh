e0f04e7647caa4f7b962a39eb99a727f74e4a6d9
Huang Wei <wei.huang@hypereal.com>
2020-03-01 16:04:52 +0800

    adopt for subdir when make template

--- mk-tpl.sh
+++ mk-tpl.sh
@@ -11,5 +11,10 @@ question__title_slug=`cat problems.json \
 echo $question__title_slug
 key="${problemId}-${question__title_slug}"
 
-mkdir -p "src/${key}"
-cp src/template.go "src/${key}/${key}_test.go"
+idLength=${#problemId}
+subDirs=("1+" "10+" "100+" "1000+")
+subDir="${subDirs[idLength - 1]}"
+dirBase="src/${subDir}"
+
+mkdir -p "${dirBase}/${key}"
+cp src/template.go "${dirBase}/${key}/${key}_test.go"

96062ba466ddde7b369937c7b9f2d05abfdda814
Huang Wei <huangwei.a180285@gmail.com>
2020-03-10 19:31:12 +0800

    igore build folder

--- build/third-party/googletest-release-1.10.0/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/huangwei/work/leetcode-com/third-party/googletest-release-1.10.0/googletest
-# Build directory: /Users/huangwei/work/leetcode-com/build/third-party/googletest-release-1.10.0/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
